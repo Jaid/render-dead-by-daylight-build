9ac6635b8ed55681e03482430bb8dc57136ea46f
Jaid <jaid.jsx@gmail.com>
2020-03-25 07:57:13 +0100

    docs: Updated readme source

--- readme/example.js
+++ readme/example.js
@@ -1,3 +1,8 @@
 import renderDeadByDaylightBuild from "render-dead-by-daylight-build"
+import Jimp from "jimp"
 
-const result = renderDeadByDaylightBuild()
+const perkIds = ["wellMakeIt", "noMither", "selfCare", "headOn"]
+const screenshotBuffer = await renderDeadByDaylightBuild(perkIds)
+
+const jimp = await Jimp.read(screenshotBuffer)
+await jimp.writeAsync("./build.png")
