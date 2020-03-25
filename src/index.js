45f75985dddc719d79b72f39a05871a55ea94744
Jaid <jaid.jsx@gmail.com>
2020-03-25 07:53:58 +0100

    Initial implementation
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 /** @module render-dead-by-daylight-build */
 
+import delay from "delay"
+import puppeteer from "puppeteer"
+
+const selector = "#showcase"
+
 /**
- * @returns {number} Seconds passed since Unix epoch (01 January 1970)
+ * @returns {Buffer}
  */
-export default () => {
-  return Math.floor(Date.now() / 1000)
+export default async objectIds => {
+  let browser
+  try {
+    browser = await puppeteer.launch({
+      defaultViewport: {
+        width: 1200,
+        height: 1200,
+      },
+      devtools: false,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--font-render-hinting=medium",
+        "--enable-font-antialiasing",
+      ],
+    })
+    const page = await browser.newPage()
+    await page.goto(`https://entity.works/showcase/${objectIds.join(",")}`, {waitUntil: "domcontentloaded"})
+    await page.waitForSelector(selector)
+    await delay(1000)
+    const element = await page.$(selector)
+    const buffer = await element.screenshot({
+      omitBackground: true,
+    })
+    await browser.close()
+    return buffer
+  } catch (error) {
+    console.error("Failed to run %s", error)
+  }
+  await browser?.close()
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,6 @@
+import fsp from "@absolunet/fsp"
+import Jimp from "jimp"
+import ms from "ms.macro"
 import path from "path"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
@@ -7,7 +10,13 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
  */
 const {default: renderDeadByDaylightBuild} = indexModule
 
-it("should run", () => {
-  const result = renderDeadByDaylightBuild()
-  expect(result).toBeGreaterThan(1549410770)
-})
+it("should run", async () => {
+  const outputFolder = path.resolve(__dirname, "..", "dist", "test")
+  const outputFile = path.join(outputFolder, "build.png")
+  await fsp.emptyDir(outputFolder)
+  const screenshotBuffer = await renderDeadByDaylightBuild(["wellMakeIt", "noMither", "selfCare", "headOn"])
+  const jimp = await Jimp.read(screenshotBuffer)
+  await jimp.writeAsync(outputFile)
+  const exists = await fsp.pathExists(outputFile)
+  expect(exists).toBeTruthy()
+}, ms`30 seconds`)
